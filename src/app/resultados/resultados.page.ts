4c0c2bfa55cbdba9223ffa7939aef3d16ceb7d90
KELVIN MENDES RIBEIRO <kelvin.ribeiro1@alunos.sc.senac.br>
2023-10-19 09:38:06 -0300

    ndaerro

--- src/app/resultados/resultados.page.ts
+++ src/app/resultados/resultados.page.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-resultados',
-  templateUrl: './resultados.page.html',
-  styleUrls: ['./resultados.page.scss'],
+  templateUrl: 'resultados.page.html',
+  styleUrls: ['resultados.page.scss'],
 })
-export class ResultadosPage implements OnInit {
+export class ResultadosPage {
+  resultados = [
+    { ano: '2023', mes: 'Janeiro', pdfUrl: 'URL_DO_PDF_JANEIRO' },
+    { ano: '2023', mes: 'Fevereiro', pdfUrl: 'blob:https://pdfviewer.softgateon.net/8715a0c0-1fa1-4afd-a6be-56adc457ef24' },
+    { ano: '2023', mes: 'Março', pdfUrl: 'URL_DO_PDF_MARCO' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  abrirPDF(pdfUrl: string) {
+    window.open(pdfUrl, '_system');
   }
-
 }

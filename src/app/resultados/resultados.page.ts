4ef72f18868f67e1ead6efc06a12198cd4c6e05d
KELVIN MENDES RIBEIRO <kelvin.ribeiro1@alunos.sc.senac.br>
2023-10-19 10:58:31 -0300

    maisatualizadoqbombapatch

--- src/app/resultados/resultados.page.ts
+++ src/app/resultados/resultados.page.ts
@@ -7,9 +7,7 @@ import { Component } from '@angular/core';
 })
 export class ResultadosPage {
   resultados = [
-    { ano: '2023', mes: 'Janeiro', pdfUrl: 'URL_DO_PDF_JANEIRO' },
-    { ano: '2023', mes: 'Fevereiro', pdfUrl: 'blob:https://pdfviewer.softgateon.net/8715a0c0-1fa1-4afd-a6be-56adc457ef24' },
-    { ano: '2023', mes: 'Março', pdfUrl: 'URL_DO_PDF_MARCO' },
+    { ano: '2023', mes: 'Fevereiro', pdfUrl: 'https://www.flipsnack.com/C876F75569B/fevereiro-2023/full-view.html' },
   ];
 
   constructor() {}

72b5b034dd09b796ff9d3f587a8b56d82d16097f
GABRIEL MARTINS VIEIRA <gabriel.vieira2@alunos.sc.senac.br>
2023-10-20 11:15:59 -0300

    metade

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+ 
+
 @Component({
+
   selector: 'app-home',
+
   templateUrl: 'home.page.html',
+
   styleUrls: ['home.page.scss'],
+
 })
+
 export class HomePage {
-  constructor() {}
+
+  constructor(private router: Router) {}
+
+  navigateToHoleritePage() {
+
+    this.router.navigate(['/holerite-page']);
+
+  }
+
+  navigateToFolhaPagamento() {
+
+    this.router.navigate(['/folha-pagamento']);
+
+  }
+
+  navigateToUsuarioForm() {
+
+    this.router.navigate(['/usuario-form']);
+
+  }
+
 }

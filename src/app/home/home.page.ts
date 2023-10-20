2745988457973c3d4b89d05207a2f3e591501633
MARCUS VINICIUS PADILHA MARCELLO <marcus.marcello@alunos.sc.senac.br>
2023-10-20 11:07:16 -0300

    linkagem

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private router: Router) {}
+  navigateToHoleritePage() {
+    this.router.navigate(['/holerite-page']);
+  }
+  navigateToFolhaPagamento() {
+    this.router.navigate(['/folha-pagamento']);
+  } 
+  navigateToUsuarioForm() {
+    this.router.navigate(['/usuario-form']);
+  }
 }
